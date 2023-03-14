3e9209d45ecc1f0de2eca12a9c9e4a81f1031f37
Yanina Rodriguez <rodriguezyanina169@gmail.com>
2023-03-14 13:30:36 -0300

    corrijo variables

--- js/ejercicio-catorce.js
+++ js/ejercicio-catorce.js
@@ -1,11 +1,11 @@
 // Realiza un script que pida una cadena de texto y lo muestre poniendo el signo – entre cada carácter sin usar el método replace. Por ejemplo, si tecleo “hola qué tal”, deberá salir “h-o-l-a- -q-u-e- -t-a-l”.
 
-var cadena = prompt("Ingresa una cadena de texto:");
+let cadena = prompt("Ingresa una cadena de texto:");
 
 
-var resultado = "";
+let resultado = "";
 
-for (var i = 0; i < cadena.length; i++) {
+for (let i = 0; i < cadena.length; i++) {
   resultado += cadena.charAt(i);
   if (i !== cadena.length - 1) {
     resultado += "-";
