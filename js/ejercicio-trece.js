5f6c684e6627a879a4f109867a86a63368766a64
Yanina Rodriguez <rodriguezyanina169@gmail.com>
2023-03-13 23:22:34 -0300

    Agrego ejercicio trece

---
+++ js/ejercicio-trece.js
@@ -0,0 +1,10 @@
+// Realiza un script que pida un texto y lo muestre en mayúsculas.
+
+let frase = prompt('Ingrese su frase');
+
+console.log(frase.toLocaleUpperCase());
+
+frase = frase.toLocaleUpperCase();
+
+document.write(frase);
+
