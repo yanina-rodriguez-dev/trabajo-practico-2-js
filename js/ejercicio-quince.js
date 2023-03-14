01c2420ecb5ea5b387ef117e78d34d600325aa3e
Yanina Rodriguez <rodriguezyanina169@gmail.com>
2023-03-13 23:37:50 -0300

    ejercicio quince comienzo

---
+++ js/ejercicio-quince.js
@@ -0,0 +1,9 @@
+//Realiza un script que cuente el número de vocales que tiene un texto.
+
+let frase = prompt('Ingrese su frase');
+
+console.log(frase.toLocaleLowerCase)
+frase =
+console.log(frase.length);
+
+if(frase.charAt(0) === 'a'|| frase.charAt(0) === 'e' || frase.charAt(0) === 'i' || frase.charAt(0) === 'o' || frase.charAt(0) === 'u')
